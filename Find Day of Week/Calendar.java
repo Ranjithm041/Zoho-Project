401228f6e2b7d5b644201efd7f13dcd814738fd2
ranjithm041 <mranjith901@gmail.com>
2024-09-18 13:14:31 +0530

    Add comments

--- Find Day of Week/Calendar.java
+++ Find Day of Week/Calendar.java
@@ -1,27 +1,18 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-class Date {
+final class Date {
     private int date;
     private String month;
     private int year;
-    private String dayOfWeek;
+    
+// Constructor for initialize the date, month,year
     public Date(int date,String month,int year){
-        
         this.date=date;
         this.month=month;
         this.year=year;
-        this.dayOfWeek=getDayOfWeek();
     }
-    public void setDate(int date){
-        this.date=date;
-    }
-    public void setYear(int year){
-        this.year=year;
-    }
-    public void setMonth(String month){
-        this.month=month;
-    }
+// Getter methods for date, year, month
     public int getDate(){
         return date;
     }
@@ -31,32 +22,52 @@ class Date {
     public String getMonth(){
         return month;
     }
+// Setter methods for date, year, month
+    public void setDate(int date){
+        this.date=date;
+    }
+    public void setYear(int year){
+        this.year=year;
+    }
+    public void setMonth(String month){
+        this.month=month;
+    }
+// Methode to calculate day of week
     public  String getDayOfWeek(){
         
         int yearCode = year%100;
         int leapYearCount = getLeapYearCount(year);
         int monthCode = getMonthCode(month);
         int centuryCode = getCenturyCode(year);
-        int sum = (yearCode + leapYearCount + monthCode + date + centuryCode) % 7;
+        int sum = (yearCode + leapYearCount + monthCode + date + centuryCode) % 7; // thisi is the formula 
         String[] daysOfWeek = {"sunday", "monday", "tuesday", "wednesday", "thursday", "friday","saturday"};
-       return daysOfWeek[sum];
+    return daysOfWeek[sum];
+        
+       
     }
-    public String getDay(){
-        return this.dayOfWeek;
-    }
-    public boolean isValidMonth(int date, String month, int year) {
+
+// Method to check the date is valid or not
+    public boolean isValidDate(int date, String month, int year) {
         int maxDaysInMonth = getMonthDuration(month);
         return date > 0 && date <= maxDaysInMonth;
     }
+
+// Method to calculate century code
     public  int  getCenturyCode(int year){
      return (6-((year/100)%4)*2)%7;
     }
+
+// Method to calculate  leapYear count 
     public  int getLeapYearCount(int year){
         return (year%100)/4;
     }
+
+// method to check is the year leap year or not 
     public boolean isLeapYear(int year){
         return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
     }
+
+//Method to get month code 
     public int getMonthCode(String month) {
         switch (month) {
             case "jan": return isLeapYear(year) ? 6: 0;
@@ -71,17 +82,18 @@ class Date {
             case "oct": return 0;
             case "nov": return 3;
             case "dec": return 5;
-            default: System.out.println("Invalid month: " + month); break;
         }
         return -1;
     }
+
+// Method to get max days in month
     public int getMonthDuration(String month){
         switch(month){
-            case "jan": return 31;
+            case "jan": return 31; 
             case "feb": return isLeapYear(year) ? 29 : 28; 
-            case "mar": return 31;
-            case "apr": return 30;
-            case "may": return 31;
+            case "mar": return 31; 
+            case "apr": return 30; 
+            case "may": return 31; 
             case "jun": return 30;
             case "jul": return 31;
             case "aug": return 31;
@@ -89,19 +101,20 @@ class Date {
             case "oct": return 31;
             case "nov": return 30;
             case "dec": return 31;
-            default: System.out.println("Invalid month: " + month); break;
         }
         return -1;
     }
+
+// Method to print monnth in calendar format
     public void printMonth(String month,int year){
         setDate(1);
         setMonth(month);
         setYear(year);
         String startDate =getDayOfWeek();
         String days[]={"sunday","monday","tuesday","wednesday","thursday","friday","saturday"};
-        int count=0;
-        int num=1;
- 
+        int count=0, num=1;
+
+        // display month and year title format
          System.out.printf("""
                           ***************************
                                    %s %d            
@@ -109,6 +122,7 @@ class Date {
                           """,
                  month, year
          );
+         //print header days and store the position of startdate day in count variable
          for (int i = 0; i < days.length; i++) {
             if(days[i].equals(startDate)){
             count=i;
@@ -116,6 +130,7 @@ class Date {
             System.out.printf("%3s ",days[i].substring(0,3));
         }
         System.out.println();
+         // print dates according from the startdate day to end
         for (int i = 1; i <=6; i++) {
            
            for (int j = 1; j <= 7; j++) {
@@ -138,28 +153,34 @@ public class Calendar{
         String month;
         int year ;
         try{
-            System.out.print("Enter the Date : ");
-            date = input.nextInt();
-            input.nextLine();
-            System.out.print("Enter the Month : ");
-            month = input.nextLine().trim().toLowerCase().substring(0,3);
-            System.out.print("Enter the year : ");
-            year = input.nextInt();
-            System.out.println();
-            input.close();
-
-            Date d = new Date(date, month, year);
-            if (! d.isValidMonth(date, month, year)) {
-                System.out.println("Invalid date, month and year....");
-                return;
+            while(true){
+                System.out.print("Enter the Date : ");
+                date = input.nextInt();
+                input.nextLine();
+                System.out.print("Enter the Month : ");
+                month = input.nextLine().trim().toLowerCase().substring(0,3);
+                System.out.print("Enter the year : ");
+                year = input.nextInt();
+                System.out.println();
+                Date d = new Date(date, month, year);
+                if (! d.isValidDate(date, month, year)) {
+                    System.out.println("Invalid date, month and year....");
+                    continue;
+                }
+                String day = d.getDayOfWeek(); 
+                System.out.printf(" %d  %s  %d  is : %s \n\n",date ,month , year , day);
+                d.printMonth(month, year);
+                System.out.print("Enter -1 to exit / 0 to continue : ");
+                int exit = input.nextByte();
+                if(exit==-1){
+                    input.close();
+                    return;
+                }
             }
-            String day = d.getDay(); 
-            System.out.printf(" %d  %s  %d  is : %s \n\n",date ,month , year , day);
-            d.printMonth(month, year);
+           
         }catch (InputMismatchException e) {
             System.out.println("Invalid input! Please enter a valid data " + e);
-        }finally{
-            input.close();
+            input.nextLine();
         }
        
     }
