7fe7dcbcf7eae47413ac964d819f1da1f2c483d4
ranjithm041 <mranjith901@gmail.com>
2024-09-18 15:56:27 +0530

    Add comments and update few changes

--- 15 Number Puzzle/Game.java
+++ 15 Number Puzzle/Game.java
@@ -2,10 +2,12 @@
 import java.util.Scanner;
 
 
- class FifteenPuzzle{
-  private static  int matrix[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
-  private static  int rowOfZero ;
-  private static  int columnOfZero ;
+final class FifteenPuzzle{
+  private final  int matrix[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,0,15}}; //initialize the board
+  private  int rowOfZero ;
+  private  int columnOfZero ;
+
+// Method to move upward  
 public void moveUp(){
     findZero();
     if (rowOfZero>0) {
@@ -18,6 +20,8 @@ public void moveUp(){
         System.out.println("No possible move for up ");
     }
 }
+
+// Method to move down
 public  void moveDown(){
     findZero();
     if (rowOfZero<matrix.length-1) {
@@ -31,6 +35,7 @@ public  void moveDown(){
     }
 }
 
+//Method to move right
 public  void moveRight(){
      findZero();
         if (columnOfZero < matrix[0].length - 1) {
@@ -43,6 +48,8 @@ public  void moveRight(){
                 System.out.println("No possible for move right ");
             }
 }
+
+// Method to move Left
 public  void moveLeft(){
     findZero();
     if(columnOfZero>0){
@@ -55,6 +62,8 @@ public  void moveLeft(){
         System.out.println("Not possible for move left ");
     }
 }
+
+// Method to Locate the Zero
 public  void findZero(){
     for (int i = 0; i < matrix.length; i++) {
         for (int j = 0; j < matrix[i].length; j++) {
@@ -67,6 +76,8 @@ public  void findZero(){
     }
     
 }
+
+// Method to print a Board
 public  void printBoard(){
     System.out.println();
     for(int i = 0; i < matrix.length; i++) {
@@ -76,6 +87,8 @@ public  void printBoard(){
         System.out.println();
     }
 }
+
+// Method to shuffle the board
 public  void shuffleBoard() {
     int dummy[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
     for (int i = 0; i < dummy.length; i++) {
@@ -92,7 +105,8 @@ public  void shuffleBoard() {
         }
     }
 }
-    
+
+// Method to the current board match the pattern or not
     public  boolean isSolved(){
         int pattern[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
         for (int i = 0; i < pattern.length; i++) {
@@ -110,21 +124,36 @@ public class Game{
     public static void main(String[] args) {
         FifteenPuzzle puzzle = new FifteenPuzzle();
         Scanner input = new Scanner(System.in);
-        // puzzle.shuffleBoard(); 
+        puzzle.shuffleBoard(); 
         puzzle.printBoard();
-        while (!puzzle.isSolved()) {
-            System.out.print("Enter move (up/down/left/right): ");
+        boolean gameOver = false;
+        while (!gameOver) {
+            System.out.print("Enter move (up-> W /down_> S /left-> A /right-> D): ");
             String move = input.nextLine().trim().toLowerCase();
             switch (move) {
-                case "up": puzzle.moveUp(); break;
-                case "down": puzzle.moveDown(); break;
-                case "left": puzzle.moveLeft(); break;
-                case "right" : puzzle.moveRight(); break;
-                default: throw new  IllegalArgumentException("Invalid input... please enter only  up/down/left/right keyword "+ move);
+                case "w": puzzle.moveUp(); break;
+                case "s": puzzle.moveDown(); break;
+                case "a": puzzle.moveLeft(); break;
+                case "d" : puzzle.moveRight(); break;
+                default: System.out.println("Invalid input... please enter only  w/s/a/d keyword "); 
+            }
+            if (puzzle.isSolved()) {
+                System.out.println("Congratulations you Won the game! ");
+                System.out.print("If you want to continue the game press 0 else -1 : ");
+                byte exitStatus = input.nextByte();
+                if(exitStatus==0){
+                    gameOver=false;
+                    input.nextLine();
+                    puzzle.shuffleBoard();
+                    puzzle.printBoard();
+                }else{
+                    gameOver=true;
+                    input.close();
+                    System.out.println("exiting game....");
+                }
             }
         }
-        if (puzzle.isSolved()) 
-        System.out.println("Congratulations you Won the game! ");
+        
         
     }
 }
