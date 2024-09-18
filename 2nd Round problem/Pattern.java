f5bab9a61113976345d0049846cedcc3a2ee6c54
ranjithm041 <mranjith901@gmail.com>
2024-09-18 16:19:20 +0530

    commit

--- 2nd Round problem/Pattern.java
+++ 2nd Round problem/Pattern.java
@@ -3,7 +3,7 @@ public class Pattern{
         int row =9;
         Pattern(row);
     }
-    public static void Pattern(int row ){
+    public static void Pattern(int row){
         for (int i = 1; i <=row; i++) {
             int number=i;
             for (int j = 1; j <=i; j++) {
